3707c23e6b8ce7a493cd19620b18546d9d0792b6
VVKot <volodymyr.kot.ua@gmail.com>
2021-01-07 21:29:05 +0000

    feat: add more events to statusline autocmd

--- config/nvim/lua/vkot/statusline.lua
+++ config/nvim/lua/vkot/statusline.lua
@@ -6,14 +6,14 @@ local sections = require('el.sections')
 local subscribe = require('el.subscribe')
 local lsp_statusline = require('el.plugins.lsp_status')
 
-local maybe_coc_status = subscribe.buf_autocmd("el_coc_status", "BufRead,CursorHold", function(_, buffer)
+local maybe_coc_status = subscribe.buf_autocmd("el_coc_status", "BufNewFile,BufRead,CursorHold,CursorHoldI,InsertLeave", function(_, buffer)
   if buffer.lsp or not vim.g.did_coc_loaded then
     return ''
   end
   return vim.fn['coc#status']()
 end)
 
-local file_icon = subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+local file_icon = subscribe.buf_autocmd("el_file_icon", "BufNewFile,BufRead", function(_, bufnr)
   local icon = extensions.file_icon(_, bufnr)
   if icon then
     return icon .. ' '
