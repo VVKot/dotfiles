16041f37e5e4f1378a3f07435e1dfdc9fc6430b6
VVKot <volodymyr.kot.ua@gmail.com>
2021-01-12 05:22:51 +0000

    feat: add quickfix interactions for telescope

--- config/nvim/lua/vkot/telescope.lua
+++ config/nvim/lua/vkot/telescope.lua
@@ -6,6 +6,10 @@ local telescope_mappings = {
   ["<C-j>"] = telescope_actions.move_selection_next,
   ["<C-k>"] = telescope_actions.move_selection_previous,
   ["<esc>"] = telescope_actions.close,
+
+  ["<tab>"] = telescope_actions.toggle_selection,
+  ["<C-q>"] = telescope_actions.send_to_qflist,
+  ["<M-q>"] = telescope_actions.send_selected_to_qflist,
 }
 
 telescope.setup {
