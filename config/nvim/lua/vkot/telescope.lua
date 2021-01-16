2d207f2b69f1857e79e4a46bea7febce2bb6895d
VVKot <volodymyr.kot.ua@gmail.com>
2021-01-16 10:12:43 +0000

    feat: use buffers for preview

--- config/nvim/lua/vkot/telescope.lua
+++ config/nvim/lua/vkot/telescope.lua
@@ -22,6 +22,9 @@ telescope.setup {
       i = telescope_mappings,
       n = telescope_mappings,
     },
+    file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
   }
 }
 
