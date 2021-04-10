862c630640d567bd8f653aeb5ed9b8401a826a5f
VVKot <volodymyr.kot.ua@gmail.com>
2021-04-10 09:51:12 +0100

    feat: add default winblend for telescope

--- config/nvim/lua/plugin/telescope.lua
+++ config/nvim/lua/plugin/telescope.lua
@@ -26,6 +26,7 @@ telescope.setup {
     file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
     grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
     qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    winblend = 10,
   }
 }
 
