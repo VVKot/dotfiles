724b96a3e7232038f6db8c534592318cb43c79c2
Volodymyr Kot <vkot@palantir.com>
2021-05-24 09:55:54 +0100

    feat: don't show virtual text for diagnostics

--- config/nvim/lua/plugin/lsp.lua
+++ config/nvim/lua/plugin/lsp.lua
@@ -17,6 +17,7 @@ vim.lsp.handlers["textDocument/signatureHelp"] =
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     signs = false,
+    virtual_text = false,
   }
 )
 
