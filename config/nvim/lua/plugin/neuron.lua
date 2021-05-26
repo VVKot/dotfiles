a31b88128047e41dff999a0a504e14c7584f058f
Volodymyr Kot <vkot@palantir.com>
2021-05-26 08:03:51 +0100

    fix: only run neuron setup if it is installed

--- config/nvim/lua/plugin/neuron.lua
+++ config/nvim/lua/plugin/neuron.lua
@@ -1,10 +1,12 @@
-require'neuron'.setup {
-    virtual_titles = true,
-    mappings = true,
-    run = function()
-        vim.cmd [[set textwidth=80]]
-        vim.cmd [[set spell]]
-    end, -- function to run when in neuron dir
-    neuron_dir = "~/zettelkasten", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
-    leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
-}
+if vim.fn.executable("neuron") == 1 then
+    require"neuron".setup {
+        virtual_titles = true,
+        mappings = true,
+        run = function()
+            vim.cmd [[set textwidth=80]]
+            vim.cmd [[set spell]]
+        end, -- function to run when in neuron dir
+        neuron_dir = "~/zettelkasten", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
+        leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
+    }
+end
