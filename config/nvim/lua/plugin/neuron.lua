c134425b6f2b6f7d2084f735a6a0aed5cad8e813
Volodymyr Kot <vkot@palantir.com>
2021-06-27 08:45:55 +0100

    feat: add familiar keymap for neuron

--- config/nvim/lua/plugin/neuron.lua
+++ config/nvim/lua/plugin/neuron.lua
@@ -1,10 +1,14 @@
+local nnoremap = vim.keymap.nnoremap
+
+local neuron = require("neuron")
 if vim.fn.executable("neuron") == 1 then
-    require"neuron".setup {
+    neuron.setup {
         virtual_titles = true,
         mappings = true,
         run = function()
             vim.cmd [[set textwidth=80]]
             vim.cmd [[set spell]]
+            nnoremap {"<C-]>", function() neuron.enter_link() end}
         end, -- function to run when in neuron dir
         neuron_dir = "~/zettelkasten", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
         leader = "gz" -- the leader key to for all mappings, remember with 'go zettel'
