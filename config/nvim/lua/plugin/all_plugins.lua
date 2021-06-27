b0c32232dff567118e2771cd6d1c9a634e524f3f
Volodymyr Kot <vkot@palantir.com>
2021-06-27 06:12:21 +0100

    feat: add plugin to view remove files from Dirvish

--- config/nvim/lua/plugin/all_plugins.lua
+++ config/nvim/lua/plugin/all_plugins.lua
@@ -25,6 +25,7 @@ return require("packer").startup {
 
         -- Explorer that adheres to Vim philosophy.
         use "justinmk/vim-dirvish"
+        use "bounceme/remote-viewer"
         -- Readonly tree view.
         use "kyazdani42/nvim-tree.lua"
 
