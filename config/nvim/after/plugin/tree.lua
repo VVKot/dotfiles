5886eb4a64b3263d648a81267156b99c4f9dddee
VVKot <volodymyr.kot.ua@gmail.com>
2021-10-03 15:57:08 +0100

    fix: up-to-date tree setup

--- config/nvim/after/plugin/tree.lua
+++ config/nvim/after/plugin/tree.lua
@@ -1,15 +1,23 @@
 local nnoremap = vim.keymap.nnoremap
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-require'nvim-tree'.setup {update_to_buf_dir = false}
+local bindings = {
+    {mode = "n", key = "i", cb = tree_cb("edit")},
+    {mode = "n", key = "a", cb = tree_cb("vsplit")},
+    {mode = "n", key = "o", cb = tree_cb("split")},
+    {mode = "n", key = "p", cb = tree_cb("preview")}
+}
+require'nvim-tree'.setup {
+    update_to_buf_dir = {enable = false, auto_open = false},
+    view = {
+        width = 60,
+        side = "left",
+        mappings = {custom_only = true, list = bindings}
+    }
+}
 
 nnoremap {
     "<Leader>tf", function() vim.api.nvim_command [[NvimTreeFindFile]] end
 }
 
-vim.g.nvim_tree_width = 60
 vim.g.nvim_tree_disable_window_picker = 1
-vim.g.nvim_tree_bindings = {
-    {key = "i", cb = tree_cb("edit")}, {key = "a", cb = tree_cb("vsplit")},
-    {key = "o", cb = tree_cb("split")}, {key = "p", cb = tree_cb("preview")}
-}
