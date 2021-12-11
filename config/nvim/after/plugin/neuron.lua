4a84e21d1cba6ed7d7d6687e4a1ebbde500cf7f2
VVKot <volodymyr.kot.ua@gmail.com>
2021-12-11 16:32:31 +0000

    local: add random zettel opener

--- config/nvim/after/plugin/neuron.lua
+++ config/nvim/after/plugin/neuron.lua
@@ -18,6 +18,14 @@ if vim.fn.executable "neuron" == 1 then
         "<C-t>",
         "<C-o>",
       }
+
+      -- note: local fork required
+      nnoremap {
+        "gzr",
+        function()
+          neuron.open_random()
+        end,
+      }
     end, -- function to run when in neuron dir
     neuron_dir = "~/zettelkasten", -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
     leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
