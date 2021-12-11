32148c0becfa41dde3b56ae95328c4a5a70206b4
VVKot <volodymyr.kot.ua@gmail.com>
2021-12-11 14:20:51 +0000

    feat: set up gradle test

--- config/nvim/after/plugin/vim-test.lua
+++ config/nvim/after/plugin/vim-test.lua
@@ -1,12 +1,23 @@
 local nnoremap = vim.keymap.nnoremap
 
+vim.g["test#java#runner"] = "gradletest"
 vim.g["test#javascript#runner"] = "jest"
 vim.g["test#strategy"] = "neovim"
 vim.g["test#javascript#jest#options"] = { all = "--silent" }
 
 local last_test_cwd = ""
 
+local does_file_require_changing_cwd = function()
+  local ft = vim.bo.filetype
+  return ft == "javascript" or ft == "typescript"
+end
+
 local call_with_test_directory_as_cwd = function(command)
+  if not does_file_require_changing_cwd() then
+    vim.api.nvim_command(command)
+    return
+  end
+
   local cwd = vim.api.nvim_eval "getcwd()"
   vim.o.autochdir = true
   last_test_cwd = vim.api.nvim_eval "getcwd()"
@@ -16,6 +27,11 @@ local call_with_test_directory_as_cwd = function(command)
 end
 
 local call_with_last_test_cwd = function(command)
+  if not does_file_require_changing_cwd() then
+    vim.api.nvim_command(command)
+    return
+  end
+
   local cwd = vim.api.nvim_eval "getcwd()"
   vim.api.nvim_command("cd " .. last_test_cwd)
   vim.api.nvim_command(command)
