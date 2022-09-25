bd44cd3c523b47f644439fa51dcad8bd5b0612d6
VVKot <volodymyr.kot.ua@gmail.com>
2022-09-25 13:07:18 +0100

    chore: remove custom filename func

--- config/nvim/after/plugin/statusline.lua
+++ config/nvim/after/plugin/statusline.lua
@@ -1,14 +1,3 @@
--- Needs a custom function to prevent unnecessary shortening of the path
-local function full_filename()
-  local data = vim.fn.expand("%:~:.")
-  if vim.bo.modified then
-    data = data .. " [+]"
-  elseif vim.bo.modifiable == false or vim.bo.readonly == true then
-    data = data .. " [-]"
-  end
-  return data
-end
-
 require("lualine").setup({
   options = {
     theme = "github_vkot",
@@ -17,7 +6,7 @@ require("lualine").setup({
   sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { { full_filename } },
+    lualine_c = { { "filename", path = 1 } },
     lualine_x = { "diagnostics" },
     lualine_y = { "encoding", "fileformat", "filetype" },
     lualine_z = { "location" },
@@ -25,7 +14,7 @@ require("lualine").setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { { full_filename } },
+    lualine_c = { { "filename", path = 1 } },
     lualine_x = { "diagnostics" },
     lualine_y = {},
     lualine_z = { "location" },
