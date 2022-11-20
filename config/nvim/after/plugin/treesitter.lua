e1917fdfc8d77034892f249e52af6e651f1165cb
VVKot <volodymyr.kot.ua@gmail.com>
2022-11-20 16:57:05 +0000

    chore: remove unused mappings

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -21,19 +21,15 @@ treesitter.setup({
       set_jumps = true,
       goto_next_start = {
         ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
       },
       goto_next_end = {
         ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
       },
       goto_previous_start = {
         ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
       },
       goto_previous_end = {
         ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
       },
     },
   },
