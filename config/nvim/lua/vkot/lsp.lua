8bebd568774f533caaeec21fe8acdb7b6fcc8e0b
VVKot <volodymyr.kot.ua@gmail.com>
2023-12-20 09:18:27 +0000

    feat: set listchars

--- config/nvim/lua/vkot/lsp.lua
+++ config/nvim/lua/vkot/lsp.lua
@@ -1,3 +1,4 @@
+vim.opt.listchars = { tab = "⇥ ", leadmultispace = "┊ ", trail = "␣", nbsp = "⍽" }
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
