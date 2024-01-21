9f783642b8e6981fc4263f439020b00414d8079e
VVKot <volodymyr.kot.ua@gmail.com>
2024-01-21 17:13:30 +0000

    feat: no virtual text for diagnostics

--- config/nvim/after/plugin/ale.vim
+++ config/nvim/after/plugin/ale.vim
@@ -1,1 +1,2 @@
 let g:ale_use_neovim_diagnostics_api = 1
+let g:ale_virtualtext_cursor = 0
--- config/nvim/lua/vkot/lsp.lua
+++ config/nvim/lua/vkot/lsp.lua
@@ -1,8 +1,7 @@
 vim.opt.listchars = { tab = "⇥ ", leadmultispace = "┊ ", trail = "␣", nbsp = "⍽" }
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+vim.diagnostic.config({ virtual_text = false })
 
 local lspconfig = require("lspconfig")
 
