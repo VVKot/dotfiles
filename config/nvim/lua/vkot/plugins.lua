6ac5e096a16a8c62e0b1490144ddd6ef92666150
VVKot <volodymyr.kot.ua@gmail.com>
2024-02-04 18:12:39 +0000

    feat: move config and add load condition for obsidian

--- config/nvim/lua/vkot/plugins.lua
+++ config/nvim/lua/vkot/plugins.lua
@@ -79,6 +79,29 @@ local plugins = {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    cond = function()
+      return vim.fn.isdirectory(".obsidian") ~= 0
+    end,
+    opts = {
+      detect_cwd = true,
+      open_app_foreground = true,
+
+      completion = {
+        nvim_cmp = false,
+      },
+
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+      },
+      note_id_func = function()
+        return os.date("%Y%m%d%H%M", os.time())
+      end,
+      disable_frontmatter = true,
+
+      mappings = {},
+    },
   },
 
   -- Copilot
