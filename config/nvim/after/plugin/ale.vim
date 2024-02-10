f1920db14021ed4ef21d642ec7af5a78be44f264
VVKot <volodymyr.kot.ua@gmail.com>
2024-02-10 16:25:46 +0000

    feat: disable ale's signs

--- config/nvim/after/plugin/ale.vim
+++ config/nvim/after/plugin/ale.vim
@@ -1,2 +1,3 @@
 let g:ale_use_neovim_diagnostics_api = 1
 let g:ale_virtualtext_cursor = 0
+let g:ale_set_signs = 0
--- config/nvim/lua/vkot/lsp.lua
+++ config/nvim/lua/vkot/lsp.lua
@@ -1,7 +1,6 @@
 vim.opt.listchars = { tab = "⇥ ", leadmultispace = "┊ ", trail = "␣", nbsp = "⍽" }
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
-vim.diagnostic.config({ virtual_text = false })
 
 local lspconfig = require("lspconfig")
 
