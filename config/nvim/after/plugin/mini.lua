3d0447b1af8b0be4265d782323e32f7314470741
VVKot <volodymyr.kot.ua@gmail.com>
2024-06-15 17:12:18 +0100

    feat: use mini.statusline

--- config/nvim/after/plugin/mini.lua
+++ config/nvim/after/plugin/mini.lua
@@ -24,3 +24,28 @@ require("mini.completion").setup({
 })
 
 require("mini.notify").setup()
+
+local statusline = require("mini.statusline")
+
+statusline.setup({
+  content = {
+    active = function()
+      local filename      = statusline.section_filename({ trunc_width = 140 })
+      local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+      local lsp           = statusline.section_lsp({ trunc_width = 75 })
+      local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
+      local location      = statusline.section_location({ trunc_width = 75 })
+
+      return statusline.combine_groups({
+        { hl = 'statuslineFilename', strings = { filename } },
+        '%<', -- Mark general truncate point
+        '%=', -- End left alignment
+        { hl = 'statuslineDevinfo',  strings = { diagnostics, lsp } },
+        { hl = 'statuslineFileinfo', strings = { fileinfo } },
+        { hl = 'statuslineFileinfo', strings = { location } },
+      })
+    end
+  },
+  set_vim_settings = false,
+})
+
