e2cfa48a99d451403e67c6fa5a7bf04e32da6cbc
VVKot <volodymyr.kot.ua@gmail.com>
2024-11-10 16:04:05 +0000

    feat: extra none-ls sources

--- config/nvim/after/plugin/none.lua
+++ config/nvim/after/plugin/none.lua
@@ -2,10 +2,17 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.completion.spell,
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.gomodifytags,
+    null_ls.builtins.code_actions.impl,
     null_ls.builtins.code_actions.proselint,
     null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.code_actions.ts_node_action,
+    null_ls.builtins.completion.spell,
+
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.stylua,
+
+    null_ls.builtins.hover.printenv,
   },
 })
