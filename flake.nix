16b2930bf3141119a19ca437f39208224f543326
VVKot <volodymyr.kot.ua@gmail.com>
2025-07-06 10:50:57 +0100

    chore: delete cruft

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
@@ -21,18 +21,6 @@
     home-manager,
     ...
   }: {
-    nixosConfigurations = {
-      hubbabubba = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/hubbabubba/configuration.nix
-        ];
-        specialArgs = {
-          inherit home-manager;
-        };
-      };
-    };
-
     darwinConfigurations = {
       bigapple = nix-darwin.lib.darwinSystem {
         system = "x86_64-darwin";
@@ -41,45 +29,6 @@
         ];
         specialArgs = {
           inherit self home-manager;
-        };
-      };
-    };
-
-    homeConfigurations = {
-      coder = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        modules = [
-          ./modules/home-manager/home.nix
-          ./modules/home-manager/kubernetes-dev.nix
-          ./modules/home-manager/rust-dev.nix
-        ];
-        extraSpecialArgs = {
-          vars = {
-            username = "coder";
-            home = "/home";
-            homebrewPrefix = "/home/linuxbrew/.linuxbrew";
-          };
-        };
-      };
-
-      vkot = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-        };
-        modules = [
-          ./modules/home-manager/big-data.nix
-          ./modules/home-manager/home.nix
-          ./modules/home-manager/kubernetes-dev.nix
-          ./modules/home-manager/writing.nix
-        ];
-        extraSpecialArgs = {
-          vars = {
-            username = "vkot";
-            home = "/Users";
-            homebrewPrefix = "/opt/homebrew";
-          };
         };
       };
     };
