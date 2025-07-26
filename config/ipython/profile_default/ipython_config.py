e62ec8fd6f858c46ac0e215f9b7de4a066aba25e
VVKot <volodymyr.kot.ua@gmail.com>
2025-07-26 13:59:40 +0100

    feat: enable vi mode in iPython

--- config/ipython/profile_default/ipython_config.py
+++ config/ipython/profile_default/ipython_config.py
@@ -617,7 +617,7 @@ c = get_config()  #noqa
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
 #  Default: 'emacs'
-# c.TerminalInteractiveShell.editing_mode = 'emacs'
+c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #  Default: 'nvim'
