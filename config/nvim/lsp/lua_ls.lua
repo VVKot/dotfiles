4309e8583aa8a4bc46bbbd4533a9cbdc3e2e4b58
VVKot <volodymyr.kot.ua@gmail.com>
2025-12-06 11:13:01 +0000

    fix: correct lua_ls config

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -1,7 +1,18 @@
 return {
 	settings = {
 		Lua = {
-			workspace = { checkThirdParty = false },
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = {
+					"vim",
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
 			telemetry = { enable = false },
 		},
 	},
