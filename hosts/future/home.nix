03ecdb9b1fc9b27c314bcc611b6828333b6e935b
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2025-12-25 13:30:34 +0000

    feat: setup gmail for git send-email

--- hosts/future/home.nix
+++ hosts/future/home.nix
@@ -12,4 +12,19 @@ args @ {pkgs, ...}: {
     ../../modules/nixos/media.nix
     ../../modules/nixos/zathura.nix
   ];
+
+  programs.git = {
+    settings = {
+      sendemail = {
+        smtpServer = "smtp.gmail.com";
+        smtpUser = "${args.vars.email}";
+        smtpServerPort = 587;
+        smtpEncryption = "tls";
+      };
+
+      credential = {
+        helper = "cache --timeout 3600";
+      };
+    };
+  };
 }
