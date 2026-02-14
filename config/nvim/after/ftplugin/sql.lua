5e2700c053fd502c39468e369b73ae5b89e4f6c9
VVKot <volodymyr.kot.ua@gmail.com>
2026-02-14 17:06:50 +0000

    feat: add SQL shortcuts

--- config/nvim/after/ftplugin/sql.lua
+++ config/nvim/after/ftplugin/sql.lua
@@ -1,1 +1,7 @@
 vim.bo.omnifunc = "vim_dadbod_completion#omni"
+
+-- mirror SSMS
+vim.keymap.set("n", "<F5>", ":%DB<CR>", { buffer = true, silent = true, desc = "Execute SQL file" })
+vim.keymap.set("v", "<F5>", ":DB<CR>", { buffer = true, silent = true, desc = "Execute SQL selection" })
+vim.keymap.set("n", "<C-e>", ":%DB<CR>", { buffer = true, silent = true, desc = "Execute SQL file" })
+vim.keymap.set("v", "<C-e>", ":DB<CR>", { buffer = true, silent = true, desc = "Execute SQL selection" })
