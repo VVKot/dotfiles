0559b90d63b0a210dee07978ff3b784f8b2477d9
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2026-05-09 09:57:05 +0100

    fix: switch to new, systemd-based impermanence config
    https://github.com/nix-community/impermanence/issues/320

--- hosts/future/configuration.nix
+++ hosts/future/configuration.nix
@@ -31,30 +31,51 @@ in {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # https://github.com/nix-community/impermanence?tab=readme-ov-file#btrfs-subvolumes
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
+  boot.initrd.systemd = {
+    enable = true; # Default in 26.05
+    services.wipe-file-systems = {
+      # Specify dependencies explicitly
+      unitConfig.DefaultDependencies = false;
+      # The script needs to run to completion before this service is done
+      serviceConfig.Type = "oneshot";
+      # This service is required for boot to succeed
+      requiredBy = ["initrd.target"];
+      # Should complete before any file systems are mounted
+      before = ["sysroot.mount"];
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+      # Wait for the disk to appear
+      requires = ["dev-root_vg-root.device"];
+      after = [
+        "dev-root_vg-root.device"
+        # Allow hibernation to resume before trying to alter any data
+        "local-fs-pre.target"
+      ];
+      script = ''
+        mkdir /btrfs_tmp
+        mount /dev/root_vg/root /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
+
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
         done
-        btrfs subvolume delete "$1"
-    }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
+  };
 
   security.sudo.extraConfig = ''
     # skip lecture after each reboot
